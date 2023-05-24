50f2b97f4089d44fe1aa4fd5e77822235668ea53
Martin Costa Daddato <martin.costa.daddato@gmail.com>
2023-05-24 09:02:34 -0300

    Pre entrega 2 Martin Costa Daddato final

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -4,7 +4,7 @@ import ItemCount from '../ItemCount/ItemCount';
 function ItemDetail({id, name, price, category, img,  stock, description} ) {
 
   return (
-    <div className='flex sm:border border-neutral-800 flex-col sm:flex-row max-w-xs sm:max-w-5xl'>
+    <div className='flex sm:border border-neutral-800 flex-col sm:flex-row max-w-xs sm:max-w-3xl'>
         <img src={img} className=' sm:w-1/2 mb-10 sm:mb-0 object-cover self-center'/>
         <div className='flex flex-col w-full p-2 md:p-5 justify-between'>
             <p className='w-full text-2xl md:text-3xl text-center mb-5 sm:mb-0'>{name}</p>
@@ -12,6 +12,7 @@ function ItemDetail({id, name, price, category, img,  stock, description} ) {
             <p className='text-sm sm:text-xl'>{description}</p>
             <h3 className='self-center text-lg md:text-3xl my-5 sm:my-0'>$<b>{price}</b></h3>
             <div className='flex flex-col justify-center'>
+                <p className='text-center mb-5'>Stock: {stock}</p>
                 <ItemCount initial={1} stock={stock}/>
                 <button className='mt-3 border border-neutral-800 border-1 py-2 rounded-md text-neutral-800 '>Añadir al Carrito</button>
             </div>
